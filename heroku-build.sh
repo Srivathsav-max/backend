d9a43b64ea4a4d67db3dc80c6d9da60d846f2e18
Jaya Raj Srivathsav Adari <73651038+Srivathsav-max@users.noreply.github.com>
2025-03-02 12:29:52 -0500

    feat: heroku dev fix

--- heroku-build.sh
+++ heroku-build.sh
@@ -3,5 +3,7 @@
 echo "📌 Running Prisma Generate..."
 go run github.com/steebchen/prisma-client-go generate
 
-echo "✅ Prisma Client Generated! Running go install..."
-go install -v -tags heroku .
+echo "✅ Prisma Client Generated!"
+
+# Ensure build script permissions
+chmod +x app
